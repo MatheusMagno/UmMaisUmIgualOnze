20a0741ed13a052921779942ef21c53ca9cffdaa
Matheus Nakata <matheusmagno7898@gmail.com>
2023-11-27 20:28:17 -0300

    Login + verificação de usuario logado

--- src/rotas.js
+++ src/rotas.js
@@ -2,17 +2,19 @@ const express = require('express');
 
 const schemaUsuario = require('./validacoes/schemaUsuario');
 const validarCorpoRequisicao = require('./intermediario/validarCorpoRequisicao');
+const verificaLogin = require('./intermediario/verificaLogin');
 
 const listarCategoria = require('./controladores/categorias');
 const { cadastrarUsuario } = require('./controladores/usuario');
+const { login } = require('./controladores/login');
 
 const rotas = express.Router();
 
 rotas.get('/categoria', listarCategoria);
 rotas.post('/usuario', validarCorpoRequisicao(schemaUsuario), cadastrarUsuario);
-// rotas.post('/login', login);
+rotas.post('/login', login);
 
-// //validação do bearer token
+rotas.use(verificaLogin)
 
 // rotas.get('/usuario', detalharPerfilUsuarioLogado);
 // rotas.put('/usuario', editarPerfilDoUsuarioLogado);
