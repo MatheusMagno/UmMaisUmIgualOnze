961b53b2c881125054db1fa3fa3dc5f60c9c4bb6
Lucas Aguiar <139440827+LukzsAv@users.noreply.github.com>
2023-11-29 14:56:37 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const rotas = require('./rotas');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 app.use(rotas);
 
 app.listen(process.env.PORT);
