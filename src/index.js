3deac0ebb49489a1356bbc7793a2ace6f8d67b37
Lucas Aguiar <139440827+LukzsAv@users.noreply.github.com>
2023-12-04 16:29:29 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const rotas = require('./rotas');
-const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
