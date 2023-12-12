b72f2317c529dabffd5c14c8f499c8dd0ee6f621
Matheus Nakata <matheusmagno7898@gmail.com>
2023-12-12 16:21:44 -0300

    correção no script, endpoint cadastrar pedido

--- src/controladores/pedidos.js
+++ src/controladores/pedidos.js
@@ -6,6 +6,7 @@ const cadasTrarPedido = async (req, res) => {
     const produtosIds = pedido_produtos.map(produto => produto.produto_id);
     const quantidadeProduto = pedido_produtos.map(quantidade => quantidade.quantidade_produto);
 
+
     try {
         const clienteExiste = await knex('clientes').where({ id: cliente_id }).first();
 
@@ -13,21 +14,35 @@ const cadasTrarPedido = async (req, res) => {
             return res.status(404).json({ mensagem: 'Cliente não encontrado' });
         }
 
-        const produtosNoEstoque = await knex.select('id', 'quantidade_estoque').from('produtos').whereIn('id', produtosIds);
+        const produtosNoEstoque = await knex.select('id', 'quantidade_estoque', 'valor').from('produtos').whereIn('id', produtosIds);
+        const valorProduto = produtosNoEstoque.map(produto => produto.valor);
 
-        if (produtosNoEstoque.length !== produtosIds.length) {
-            return res.status(404).json({ mensagem: 'Produto não existe' });
+        let valor_total = 0;
+        for (let i = 0; i < produtosIds.length; i++) {
+            valor_total += valorProduto[i] * quantidadeProduto[i]
+
         }
 
-        for (const produto of produtosNoEstoque) {
-            if (produto.quantidade_estoque < quantidadeProduto[produto.id]) {
-                return res.status(404).json({ mensagem: 'Estoque insuficiente' });
+        const pedido = await knex('pedidos').insert({ cliente_id, observacao, valor_total }).returning('*');
+
+        const pedido_id = pedido[0].id
+        console.log(pedido_id);
+        console.log(produtosNoEstoque);
+
+
+        if (produtosNoEstoque) {
+            for (let i = 0; i < produtosIds.length; i++) {
+                const produto_id = produtosIds[i];
+                const quantidade_produto = quantidadeProduto[i];
+                const valor_produto = valorProduto[i]
+
+                const pedido_produtos = await knex('pedido_produtos').insert({ pedido_id, produto_id, quantidade_produto, valor_produto })
             }
         }
 
-        const pedido = await knex('pedidos').insert({ cliente_id, observacao }).returning('*');
-        res.send(pedido)
+        res.send(pedido);
     } catch (error) {
+        console.log(error.message);
         return res.status(400).json(error.message);
     }
 }
